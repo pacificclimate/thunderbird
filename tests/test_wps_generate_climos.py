f68a078ca0d1f770a18eaaaf162f187d6706cf22
nikola-rados <nrados@uvic.ca>
2020-04-01 11:26:50 -0700

    Use kwargs

--- tests/test_wps_generate_climos.py
+++ tests/test_wps_generate_climos.py
@@ -14,40 +14,26 @@ import owslib.wps
 )
 @pytest.mark.parametrize(
     (
-        "operation",
-        "climo",
-        "resolutions",
-        "convert_longitudes",
-        "split_vars",
-        "split_intervals",
-        "dry_run",
+        "kwargs"
     ),
     [
-        ("mean", "6190", "yearly", "True", "True", "True", "False"),
-        ("std", "7100", "seasonal", "False", "False", "False", "True"),
+        ({"operation":"mean", "climo":"6190", "resolutions":"yearly", "convert_longitudes":"True", "split_vars":"True", "split_intervals":"True", "dry_run":"False"}),
+        ({"operation":"std", "climo":"7100", "resolutions":"seasonal", "convert_longitudes":"False", "split_vars":"False", "split_intervals":"False", "dry_run":"True"}),
     ],
 )
-def test_wps_gen_climos_opendap(
-    opendap,
-    operation,
-    climo,
-    resolutions,
-    convert_longitudes,
-    split_vars,
-    split_intervals,
-    dry_run,
-):
+def test_wps_gen_climos_opendap(opendap, kwargs):
     client = client_for(Service(processes=[GenerateClimos()]))
     datainputs = (
-        f"opendap=@xlink:href={opendap};"
-        f"operation={operation};"
-        f"climo={climo};"
-        f"resolutions={resolutions};"
-        f"convert_longitudes={convert_longitudes};"
-        f"split_vars={split_vars};"
-        f"split_intervals={split_intervals};"
-        f"dry_run={dry_run};"
-    )
+        "opendap=@xlink:href={0};"
+        "operation={operation};"
+        "climo={climo};"
+        "resolutions={resolutions};"
+        "convert_longitudes={convert_longitudes};"
+        "split_vars={split_vars};"
+        "split_intervals={split_intervals};"
+        "dry_run={dry_run};"
+    ).format(opendap, **kwargs)
+
     resp = client.get(
         service="wps",
         request="Execute",
