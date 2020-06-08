64afbd0e0a2f58cb797ff73daea8748e93b1e56a
Eric Yvorchuk <slim@pcic-0011.pcic.uvic.ca>
2020-06-08 16:31:47 -0700

    Rearrange tests into online/offline and add mixed cases

--- tests/test_wps_generate_prsn.py
+++ tests/test_wps_generate_prsn.py
@@ -6,93 +6,24 @@ from pywps.tests import assert_response_success
 from .common import client_for, TESTDATA
 from thunderbird.processes.wps_generate_prsn import GeneratePrsn
 
-
-@pytest.mark.online
-@pytest.mark.parametrize(
-    ("kwargs"),
-    [
-        (
-            {
-                "prec": TESTDATA["test_local_pr_nc"],
-                "tasmin": TESTDATA["test_local_tasmin_nc"],
-                "tasmax": TESTDATA["test_local_tasmax_nc"],
-                "dry_run": "False",
-            }
-        ),
-        (
-            {
-                "prec": TESTDATA["test_opendap_pr_nc"],
-                "tasmin": TESTDATA["test_opendap_tasmin_nc"],
-                "tasmax": TESTDATA["test_opendap_tasmax_nc"],
-                "dry_run": "True",
-            }
-        ),
-    ],
-)
-def test_default(kwargs):
+def run(kwargs):
     client = client_for(Service(processes=[GeneratePrsn()]))
-    datainputs = (
-        "prec=@xlink:href={prec};"
-        "tasmin=@xlink:href={tasmin};"
-        "tasmax=@xlink:href={tasmax};"
-        "dry_run={dry_run};"
-    ).format(**kwargs)
-    resp = client.get(
-        service="wps",
-        request="Execute",
-        version="1.0.0",
-        identifier="generate_prsn",
-        datainputs=datainputs,
-    )
-    assert_response_success(resp)
-
-
-@pytest.mark.online
-@pytest.mark.parametrize(
-    ("kwargs"),
-    [
-        (
-            {
-                "prec": TESTDATA["test_local_pr_nc"],
-                "tasmin": TESTDATA["test_local_tasmin_nc"],
-                "tasmax": TESTDATA["test_local_tasmax_nc"],
-                "chunk_size": "50",
-                "dry_run": "False",
-                "output_file": "prsn_test1.nc",
-            }
-        ),
-        (
-            {
-                "prec": TESTDATA["test_opendap_pr_nc"],
-                "tasmin": TESTDATA["test_opendap_tasmin_nc"],
-                "tasmax": TESTDATA["test_opendap_tasmax_nc"],
-                "chunk_size": "50",
-                "dry_run": "True",
-                "output_file": "None",
-            }
-        ),
-        (
-            {
-                "prec": TESTDATA["test_opendap_pr_nc"],
-                "tasmin": TESTDATA["test_opendap_tasmin_nc"],
-                "tasmax": TESTDATA["test_opendap_tasmax_nc"],
-                "chunk_size": "50",
-                "dry_run": "False",
-                "output_file": "prsn_test2.nc",
-            }
-        ),
-    ],
-)
-def test_runs(kwargs):
-    client = client_for(Service(processes=[GeneratePrsn()]))
-    datainputs = (
+    if "chunk_size" in kwargs.keys() and "output_file" in kwargs.keys(): # Not using default values
+        datainputs = (
         "prec=@xlink:href={prec};"
         "tasmin=@xlink:href={tasmin};"
         "tasmax=@xlink:href={tasmax};"
         "chunk_size={chunk_size};"
         "dry_run={dry_run};"
         "output_file={output_file};"
-    ).format(**kwargs)
+        ).format(**kwargs)
+    else:
+        datainputs = (
+        "prec=@xlink:href={prec};"
+        "tasmin=@xlink:href={tasmin};"
+        "tasmax=@xlink:href={tasmax};"
+        "dry_run={dry_run};"
+        ).format(**kwargs)
     resp = client.get(
         service="wps",
         request="Execute",
@@ -100,4 +31,104 @@ def test_runs(kwargs):
         identifier="generate_prsn",
         datainputs=datainputs,
     )
-    assert_response_success(resp)
+    assert_response_success(resp)    
+    
+@pytest.mark.parametrize(
+    ("kwargs"),
+    [
+        (
+            {
+                "prec": TESTDATA["test_local_pr_nc"],
+                "tasmin": TESTDATA["test_local_tasmin_nc"],
+                "tasmax": TESTDATA["test_local_tasmax_nc"],
+                "dry_run": "True",
+            }
+        ),
+    ],
+)
+def test_default_local(kwargs):
+    run(kwargs)
+
+
+@pytest.mark.parametrize(
+    ("kwargs"),
+    [
+        (
+            {
+                "prec": TESTDATA["test_opendap_pr_nc"],
+                "tasmin": TESTDATA["test_opendap_tasmin_nc"],
+                "tasmax": TESTDATA["test_opendap_tasmax_nc"],
+                "chunk_size": "50",
+                "dry_run": "True",
+                "output_file": "prsn_test_local.nc",
+            }
+        ),
+    ],
+)
+def test_run_local(kwargs):
+    run(kwargs)
+
+@pytest.mark.online
+@pytest.mark.parametrize(
+    ("kwargs"),
+    [
+        (
+            {
+                "prec": TESTDATA["test_opendap_pr_nc"],
+                "tasmin": TESTDATA["test_opendap_tasmin_nc"],
+                "tasmax": TESTDATA["test_opendap_tasmax_nc"],
+                "dry_run": "True",
+            }
+        ),
+    ],
+)
+def test_default_opendap(kwargs):
+    run(kwargs)
+
+@pytest.mark.online
+@pytest.mark.parametrize(
+    ("kwargs"),
+    [
+        (
+            {
+                "prec": TESTDATA["test_opendap_pr_nc"],
+                "tasmin": TESTDATA["test_opendap_tasmin_nc"],
+                "tasmax": TESTDATA["test_opendap_tasmax_nc"],
+                "chunk_size": "50",
+                "dry_run": "False",
+                "output_file": "prsn_test_opendap.nc",
+            }
+        ),
+    ],
+)
+def test_run_opendap(kwargs):
+    run(kwargs)
+
+@pytest.mark.online
+@pytest.mark.parametrize(
+    ("kwargs"),
+    [
+        (
+            {
+                "prec": TESTDATA["test_local_pr_nc"],
+                "tasmin": TESTDATA["test_opendap_tasmin_nc"],
+                "tasmax": TESTDATA["test_opendap_tasmax_nc"],
+                "chunk_size": "100",
+                "dry_run": "True",
+                "output_file": "prsn_test_mixed1.nc",
+            }
+        ),
+        (
+            {
+                "prec": TESTDATA["test_opendap_pr_nc"],
+                "tasmin": TESTDATA["test_local_tasmin_nc"],
+                "tasmax": TESTDATA["test_local_tasmax_nc"],
+                "chunk_size": "100",
+                "dry_run": "False",
+                "output_file": "prsn_test_mixed2.nc",
+            }
+        ),
+    ],
+)
+def test_run_mixed(kwargs):
+    run(kwargs)
