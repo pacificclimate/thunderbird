69c50cb4a8e4a09ef6e4f58853b0be61a81679e9
Eric Yvorchuk <slim@pcic-0011.pcic.uvic.ca>
2020-06-19 15:49:47 -0700

    Add demo for wps_split_merged_climos

--- thunderbird/processes/__init__.py
+++ thunderbird/processes/__init__.py
@@ -2,10 +2,12 @@ from .wps_generate_climos import GenerateClimos
 from .wps_generate_prsn import GeneratePrsn
 from .wps_say_hello import SayHello
 from .wps_update_metadata import UpdateMetadata
+from .wps_split_merged_climos import SplitMergedClimos
 
 processes = [
     GenerateClimos(),
     GeneratePrsn(),
     UpdateMetadata(),
+    SplitMergedClimos(),
     SayHello(),
 ]
