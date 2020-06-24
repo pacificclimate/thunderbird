00f2a602ef3016f5cd1a1658fc14cf8e8b2cbe60
Sangwon Lim <sangwonl@uvic.ca>
2020-06-24 15:49:10 -0700

    add test_source_check

--- tests/test_wps_decompose_flow_vectors.py
+++ tests/test_wps_decompose_flow_vectors.py
@@ -6,6 +6,8 @@ from pywps.tests import assert_response_success
 from netCDF4 import Dataset
 from .common import client_for, TESTDATA, run_wps_process
 from thunderbird.processes.wps_decompose_flow_vectors import DecomposeFlowVectors
+from pywps.app.exceptions import ProcessError
+
 import owslib.wps
 import pkg_resources
 import os
@@ -44,3 +46,17 @@ def test_wps_decompose_flow_vectors_opendap(opendap, kwargs):
 def test_wps_decompose_flow_vectors_netcdf(netcdf, kwargs):
     params = build_params(netcdf, kwargs)
     run_wps_process(DecomposeFlowVectors(), params)
+
+@pytest.mark.parametrize(
+    ("netcdf"), TESTDATA["test_local_nc"],
+)
+@pytest.mark.parametrize(
+    ("kwargs"), [({"dest_file": "output.nc", "variable": "Flow_Direction",}),],
+)
+def test_source_check(netcdf, kwargs):
+    params = build_params(netcdf, kwargs)
+
+    with pytest.raises(ProcessError) as exc:
+        run_wps_process(DecomposeFlowVectors(), params)
+    
+    assert exc.value
