16180449e3f57281887e1e1eae2f9d7cfc18bd6c
Sangwon Lim <sangwonl@uvic.ca>
2020-10-08 10:10:13 -0700

    mime_type = text/x-yaml

--- thunderbird/processes/wps_update_metadata.py
+++ thunderbird/processes/wps_update_metadata.py
@@ -39,7 +39,7 @@ class UpdateMetadata(Process):
                 abstract="The filepath of an updates file that specifies what to do to the metadata it finds in the NetCDF file",
                 min_occurs=1,
                 max_occurs=1,
-                supported_formats=[Format(mime_type="text/plain", extension="yaml",)],
+                supported_formats=[Format(mime_type="text/x-yaml", extension="yaml",)],
             ),
             log_level,
         ]
